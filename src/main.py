eca3a278cd2d28365ba1725cf9a19501121ec879
Minato <takumion2000@gmail.com>
2024-07-28 01:01:09 +0900

    refactor main.py

--- src/main.py
+++ src/main.py
@@ -5,5 +5,5 @@ from config import authentication_dic
 from spotipy_utils import make_playlist_by_perfect_matching
 
 # 検索したい楽曲名を代入する
-word = "秋"
+word = "夏"
 make_playlist_by_perfect_matching(word, authentication_dic)
